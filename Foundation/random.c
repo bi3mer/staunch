6e6071ab80eed2edeb02693e98c436207bb7b402
bi3mer <colan@comcast.net>
2025-12-17 12:23:59 -0500

    started random implementation, but it is nowhere near to being complete

--- Foundation/random.c
+++ Foundation/random.c
@@ -1,2 +1,11 @@
 #include "foundation.h"
 
+#include "exam.h"
+
+// TODO: look into the modulo bias
+// TODO: so much more to implement for thi to work
+u8 f_rand_u8(const u8 min, const u8 max)
+{
+    e_assert(max >= min);
+    return min + (rand() % (max - min + 1));
+}
