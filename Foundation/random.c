1cb52c8ba936cb76aa8857edfdea461dc734fe70
bi3mer <colan@comcast.net>
2025-12-17 12:42:19 -0500

    implemented f_rand_d

--- Foundation/random.c
+++ Foundation/random.c
@@ -9,3 +9,9 @@ u8 f_rand_u8(const u8 min, const u8 max)
     e_assert(max >= min);
     return min + (rand() % (max - min + 1));
 }
+
+double f_rand_d(const double min, const double max)
+{
+    e_assert(max >= min);
+    return min + ((double)rand() / RAND_MAX) * (max - min);
+}
