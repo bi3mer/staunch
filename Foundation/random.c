8eff1bf64662d66a8586120644825787bfbff033
bi3mer <colan@comcast.net>
2025-12-18 11:34:14 -0500

    added f_rand_init and f_rand_u16. Right now we are using c's srand and rand functions but I think I will move away from both later on, but I'm more focused on convenience right now

--- Foundation/random.c
+++ Foundation/random.c
@@ -2,9 +2,22 @@
 
 #include "exam.h"
 
+#include <stdlib.h>
+
+void f_rand_init(i64 seed)
+{
+    srand(seed);
+}
+
 // TODO: look into the modulo bias
 // TODO: so much more to implement for thi to work
 u8 f_rand_u8(const u8 min, const u8 max)
+{
+    e_assert(max >= min);
+    return min + (rand() % (max - min + 1));
+}
+
+u16 f_rand_u16(const u16 min, const u16 max)
 {
     e_assert(max >= min);
     return min + (rand() % (max - min + 1));
